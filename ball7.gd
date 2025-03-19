7793c58a87c73620a2e8f808b7a1e70d322dc561
NickParamore <112004146+NickParamore@users.noreply.github.com>
2025-03-19 00:16:01 -0400

    Initial commit

---
+++ ball7.gd
@@ -0,0 +1,7 @@
+extends RigidBody2D
+
+@export var ball_type: String
+
+
+func _on_body_entered(body: Node) -> void:
+	pass # Replace with function body.
---
+++ main.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+var ball = preload("res://ball.tscn")
+var ball2 = preload("res://ball_2.tscn")
+var ball3 = preload("res://ball_3.tscn")
+var ball4 = preload("res://ball_4.tscn")
+var ball5 = preload("res://ball_5.tscn")
+var ball6 = preload("res://ball_6.tscn")
+var ball7 = preload("res://ball_7.tscn")
+var rng = RandomNumberGenerator.new()
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Left Click"):
+		inst(get_global_mouse_position())
+
+func inst(pos):
+	var ballType
+	var number = rng.randi_range(0,4)
+	if number == 0:
+		ballType = ball
+	elif number == 1:
+		ballType = ball2
+	elif number == 2:
+		ballType = ball3
+	elif number == 3:
+		ballType = ball4
+	elif number == 4:
+		ballType = ball5
+	elif number == 5:
+		ballType = ball6
+	elif number == 6:
+		ballType = ball7
+		
+	
+	var instance = ballType.instantiate()
+	instance.position = pos
+	add_child(instance)
+	
+func ballPreview():
+	var nextBall = RigidBody2D
+	
+
